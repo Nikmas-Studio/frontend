aba3470a3c89b6efba104473b9d3a6dca861fa52
Nikita Maslov <maslov.n.e@gmail.com>
2024-09-30 12:18:00 +0300

    hide header earlier

--- components/modules/Header.tsx
+++ components/modules/Header.tsx
@@ -78,7 +78,7 @@ function Header(): ReactElement {
       const scrollPosition = window.scrollY;
       const pageHeight = document.body.offsetHeight;
       const windowHeight = window.innerHeight;
-      const bottomOffset = 420;
+      const bottomOffset = 600;
 
       if (window.innerWidth < 1280) {
         if (scrollPosition + windowHeight >= pageHeight - bottomOffset) {
