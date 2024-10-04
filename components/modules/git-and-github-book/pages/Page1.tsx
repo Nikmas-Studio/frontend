8b02e5a1b0b625e01bff9171ddb83557a690f50c
Nikita Maslov <maslov.n.e@gmail.com>
2024-10-05 00:00:17 +0300

    remove unnecessary margin

--- components/modules/git-and-github-book/pages/Page1.tsx
+++ components/modules/git-and-github-book/pages/Page1.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react';
 
 function Page1(): ReactElement {
   return (
-    <section className='relative  z-30  mt-5  h-[820px]  w-full  bg-white  pt-10  dark:bg-[#00040a]'>
+    <section className='relative  z-30  h-[820px]  w-full  bg-white  pt-10  dark:bg-[#00040a]'>
       <div>
         <p className='absolute  bottom-6  right-[3.5vw]'>1</p>
       </div>
