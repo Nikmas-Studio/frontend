98389438e8605e53ef0dad1c76c029c8fbeb02ed
Nikita Maslov <maslov.n.e@gmail.com>
2024-11-01 21:17:52 +0200

    another improvement of buddha

--- app/book-master-git-and-github/page.tsx
+++ app/book-master-git-and-github/page.tsx
@@ -259,8 +259,9 @@ function MasterGitAndGithubBookPromo(): ReactElement {
                                fill-white
                                will-change-transform
                                [filter:drop-shadow(0_0_250px_rgba(0,0,0,0.20))]  md:ml-0  md:mt-20  
-                               md:w-4/5  
-                               md:max-w-[280px]
+                               md:w-4/5
+                               md:max-w-[280px]  
+                               md:[filter:drop-shadow(0_0_250px_rgba(0,0,0,0.22))]
                                dark:fill-black
                                dark:[filter:drop-shadow(0_0_250px_rgba(255,255,255,0.45))]'
                   />
