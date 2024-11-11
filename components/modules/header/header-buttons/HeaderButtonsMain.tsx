136d5927c51cd276ba6bdcd645acc7cb33c910b4
Nikita Maslov <maslov.n.e@gmail.com>
2024-11-11 07:42:44 +0200

    fix auth

--- components/modules/header/header-buttons/HeaderButtonsMain.tsx
+++ components/modules/header/header-buttons/HeaderButtonsMain.tsx
@@ -1,12 +1,18 @@
+import { checkAuth } from '@/utils/is-authenticated';
 import { ReactElement } from 'react';
 import GuestAccountIconMain from '../../../elements/header/guest-account-icon/GuestAccountIconMain';
 import ThemeToggleMain from '../theme-toggle/ThemeToggleMain';
 
 function HeaderButtonsMain(): ReactElement {
+  const isAuthenticated = checkAuth();
+
   return (
     <div className='flex  items-center  gap-x-5'>
       <ThemeToggleMain className='flex-none' />
-      <GuestAccountIconMain className='flex-none' />
+      <GuestAccountIconMain
+        isAuthenticated={isAuthenticated}
+        className='flex-none'
+      />
     </div>
   );
 }
