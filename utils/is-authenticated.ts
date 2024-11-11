5527dd594f315da40b4c7a380011830afecf76c5
Nikita Maslov <maslov.n.e@gmail.com>
2024-11-11 08:07:18 +0200

    fx

--- utils/is-authenticated.ts
+++ utils/is-authenticated.ts
@@ -1,7 +1,9 @@
 import { SESSION_ID_COOKIE_NAME } from '@/constants/general';
-import { cookies } from 'next/headers';
 
 export function checkAuth(): boolean {
-  const sessionId = cookies().get(SESSION_ID_COOKIE_NAME)?.value;
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const sessionId = require('next/headers')
+    .cookies()
+    .get(SESSION_ID_COOKIE_NAME)?.value;
   return !!sessionId;
 }
