0e3c4147696f858b842ff172d54acc7d278008c6
Nikita Maslov <maslov.n.e@gmail.com>
2024-12-06 22:43:45 +0200

    remove payment-success page

--- app/payment-success/loading.tsx
+++
@@ -1,8 +0,0 @@
-import LoadingFullPageSpinner from '@/components/elements/LoadingFullPageSpinner';
-import { ReactElement } from 'react';
-
-function Loading(): ReactElement {
-  return <LoadingFullPageSpinner />;
-}
-
-export default Loading;
--- constants/general.ts
+++ constants/general.ts
@@ -6,9 +6,6 @@ export const PAYMENT_ROUTE_AUTHENTICATED = '/api/purchase-book-authenticated';
 export const LOG_ERROR_ROUTE = '/api/log-error';
 export const GET_SESSION_ROUTE = '/api/session';
 export const VALIDATE_AUTH_TOKEN_ROUTE = '/api/auth-token/validate';
-export const PAYMENT_SUCCESS_GUEST_ROUTE = '/api/payment-success-guest';
-export const PAYMENT_SUCCESS_AUTHENTICATED_ROUTE =
-  '/api/payment-success-authenticated';
 export const UPDATE_READER_FULL_NAME_ROUTE = '/api/readers/full-name';
 export const LOGOUT_ROUTE = `/api/logout`;
 export const SESSION_ID_COOKIE_NAME = 'sessionId';
