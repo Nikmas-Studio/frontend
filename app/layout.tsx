e57cd444c3b374fb8be908b0a2a875815d1bc906
Nikita Maslov <maslov.n.e@gmail.com>
2024-12-13 16:16:04 +0200

    switch to next/script component for meta-pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,13 @@ export default function RootLayout({
             `,
           }}
         />
-        <script
+        <Script
+          id='meta-pixel'
+          strategy='afterInteractive'
           dangerouslySetInnerHTML={{
             __html: `
+              <!-- Meta Pixel Code -->
+              <script>
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -56,6 +60,27 @@ export default function RootLayout({
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '3555681048062939');
               fbq('track', 'PageView');
+              </script>
+              <!-- End Meta Pixel Code -->
+            `,
+          }}
+        />
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!-- Meta Pixel Code -->
+              <script>
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '3555681048062939');
+              fbq('track', 'PageView');
+              </script>
+              <!-- End Meta Pixel Code -->
             `,
           }}
         />
