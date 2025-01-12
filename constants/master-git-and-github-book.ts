91c83d782d442742b2b48fe388690571d3d1bc4d
Nikita Maslov <maslov.n.e@gmail.com>
2025-01-13 00:22:50 +0200

    update interactivity demo videos

--- constants/master-git-and-github-book.ts
+++ constants/master-git-and-github-book.ts
@@ -2,10 +2,10 @@ export const BASE_PATH_READ = '/book-master-git-and-github/read';
 export const BASE_PATH_DEMO = '/book-master-git-and-github/demo';
 
 export const INTERACTIVITY_DEMO_VIDEO_LIGHT_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/interactivity-demo-light-HTZNZhitKFVnmDcy0DjxkrlyRGQy79.mp4';
+  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/interactivity-demo-light-updated-CjkM71myJ5smtNZnlH6piV6zQkEoWl.mp4';
 
 export const INTERACTIVITY_DEMO_VIDEO_DARK_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/interactivity-demo-dark-4xD2MUnRl4VzoyI4lwZAak1Bd4rXMy.mp4';
+  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/interactivity-demo-dark-updated-AH42WvTh6SpB9z0P0XO9UEjK5KmMTZ.mp4';
 
 export const PAGE_NUMBERS_UPDATE_VIDEO_SRC =
   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/page-numbers-update-edmHkSnEAna17jHM2lWOOMfzP3g2og.mp4';
