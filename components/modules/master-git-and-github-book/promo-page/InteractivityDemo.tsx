7d6138e12da64324ae038d434fb25a9dbe167a56
Nikita Maslov <maslov.n.e@gmail.com>
2025-02-20 20:27:32 +0200

    update interactivity demo text fragment

--- components/modules/master-git-and-github-book/promo-page/InteractivityDemo.tsx
+++ components/modules/master-git-and-github-book/promo-page/InteractivityDemo.tsx
@@ -15,7 +15,7 @@ function InteractivityDemo(): ReactElement {
           Interactivity that&apos;s based on&nbsp;scrolling
         </H2>
         <TextNode className='lg:mb-6'>
-          Illustrations appear, pin, change, and&nbsp;disappear as&nbsp;you
+          Illustrations appear, pin, transform, and&nbsp;disappear as&nbsp;you
           scroll:
         </TextNode>
         <video
