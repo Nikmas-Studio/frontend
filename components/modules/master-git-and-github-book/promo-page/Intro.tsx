16b7988e728b402dd95e0d19fd60a9ed42257200
Nikita Maslov <maslov.n.e@gmail.com>
2025-02-21 12:06:35 +0200

    update chief director role

--- components/modules/master-git-and-github-book/promo-page/Intro.tsx
+++ components/modules/master-git-and-github-book/promo-page/Intro.tsx
@@ -94,7 +94,7 @@ function Intro(): ReactElement {
               Author:&nbsp;Nick&nbsp;Maslov
             </BasicTextNode>
             <TextNode className='w-[275px]  !text-base  lg:w-auto'>
-              CEO, Enterprise Architect, and&nbsp;Chief Director
+              CEO, Enterprise Architect, and&nbsp;Chief Book Director
               at&nbsp;Nikmas&nbsp;Studio
             </TextNode>
             <BasicTextNode
