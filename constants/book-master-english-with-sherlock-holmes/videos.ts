bec8423857afed50b9a103a8c6f20993426a0d9f
Nikita Maslov <maslov.n.e@gmail.com>
2025-07-03 23:08:35 +0300

    try hosting videos in assets

--- constants/book-master-english-with-sherlock-holmes/videos.ts
+++ constants/book-master-english-with-sherlock-holmes/videos.ts
@@ -1,29 +1,60 @@
+// export const TRANSLATION_DEMO_VIDEO_MOBILE_LIGHT_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-mobile-light-sKH1dX7Ys916vhoF3MO3d4pwvHhcb0.mp4';
+
 export const TRANSLATION_DEMO_VIDEO_MOBILE_LIGHT_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-mobile-light-sKH1dX7Ys916vhoF3MO3d4pwvHhcb0.mp4';
+  '/videos/sherlock-translation-demo-mobile-light.mp4';
+
+// export const TRANSLATION_DEMO_VIDEO_MOBILE_DARK_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-mobile-dark-uoiwdi6NbInGBq8gh843P0ijaeJTfz.mp4';
 
 export const TRANSLATION_DEMO_VIDEO_MOBILE_DARK_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-mobile-dark-uoiwdi6NbInGBq8gh843P0ijaeJTfz.mp4';
+  '/videos/sherlock-translation-demo-mobile-dark.mp4';
+
+// export const TRANSLATION_DEMO_VIDEO_DESKTOP_LIGHT_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-desktop-light-XpfStM9rNZ7byTRYn4g0CvCDcr5lDd.mp4';
 
 export const TRANSLATION_DEMO_VIDEO_DESKTOP_LIGHT_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-desktop-light-XpfStM9rNZ7byTRYn4g0CvCDcr5lDd.mp4';
+  '/videos/sherlock-translation-demo-desktop-light.mp4';
+
+// export const TRANSLATION_DEMO_VIDEO_DESKTOP_DARK_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-desktop-dark-bcPfZaZfrXJSVgg9xrINrc3Ae2WiBu.mp4';
 
 export const TRANSLATION_DEMO_VIDEO_DESKTOP_DARK_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-translation-demo-desktop-dark-bcPfZaZfrXJSVgg9xrINrc3Ae2WiBu.mp4';
+  '/videos/sherlock-translation-demo-desktop-dark.mp4';
+
+// export const NAVIGATOR_DEMO_VIDEO_MOBILE_LIGHT_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-mobile-light-dgSR5sTdqXLAfQZxdF1oFUs7JRI1WX.mp4';
 
 export const NAVIGATOR_DEMO_VIDEO_MOBILE_LIGHT_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-mobile-light-dgSR5sTdqXLAfQZxdF1oFUs7JRI1WX.mp4';
+  '/videos/sherlock-navigation-demo-mobile-light.mp4';
+
+// export const NAVIGATOR_DEMO_VIDEO_MOBILE_DARK_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-mobile-dark-vPbMRgKdysB9vhzddAZwQSMpdX4RPC.mp4';
 
 export const NAVIGATOR_DEMO_VIDEO_MOBILE_DARK_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-mobile-dark-vPbMRgKdysB9vhzddAZwQSMpdX4RPC.mp4';
+  '/videos/sherlock-navigation-demo-mobile-dark.mp4';
+
+// export const NAVIGATOR_DEMO_VIDEO_DESKTOP_LIGHT_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-desktop-light-rrfr3gOOYClZCGIiWZ4hayy0J2bP6t.mp4';
 
 export const NAVIGATOR_DEMO_VIDEO_DESKTOP_LIGHT_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-desktop-light-rrfr3gOOYClZCGIiWZ4hayy0J2bP6t.mp4';
+  '/videos/sherlock-navigation-demo-desktop-light.mp4';
+
+// export const NAVIGATOR_DEMO_VIDEO_DESKTOP_DARK_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-desktop-dark-x72gU3ojfzegnvWygHHDePPMOTPYmk.mp4';
 
 export const NAVIGATOR_DEMO_VIDEO_DESKTOP_DARK_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-navigation-demo-desktop-dark-x72gU3ojfzegnvWygHHDePPMOTPYmk.mp4';
+  '/videos/sherlock-navigation-demo-desktop-dark.mp4';
+
+// export const PAGE_NUMBERS_UPDATE_VIDEO_DESKTOP_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-page-numbers-update-AE8yA6727W9EkJgGlCrb5gOro5ZIde.mp4';
 
 export const PAGE_NUMBERS_UPDATE_VIDEO_DESKTOP_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-page-numbers-update-AE8yA6727W9EkJgGlCrb5gOro5ZIde.mp4';
+  '/videos/sherlock-page-numbers-update.mp4';
+
+// export const PAGE_NUMBERS_UPDATE_VIDEO_MOBILE_SRC =
+//   'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-page-numbers-update-mobile-Xo8d3utGOxobaIOLvAbAeb7Ff2eeXI.mp4';
 
 export const PAGE_NUMBERS_UPDATE_VIDEO_MOBILE_SRC =
-  'https://9lqg3ssfq5ex47ks.public.blob.vercel-storage.com/sherlock-page-numbers-update-mobile-Xo8d3utGOxobaIOLvAbAeb7Ff2eeXI.mp4';
+  '/videos/sherlock-page-numbers-update-mobile.mp4';
+
