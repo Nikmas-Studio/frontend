04b78ae8ce0b6bde61dde555fa5c3a636b5f2ee0
Nikita Maslov <maslov.n.e@gmail.com>
2025-07-12 20:55:33 +0300

    fix route

--- utils/purchase-book-guest-route.ts
+++ utils/purchase-book-guest-route.ts
@@ -9,7 +9,7 @@ export async function purchaseBookGuest({
   email: string;
   captchaToken: string;
 }): Promise<void> {
-  await axios.post(`api/books/${bookURI}/purchase-guest`, {
+  await axios.post(`/api/books/${bookURI}/purchase-guest`, {
     email,
     captchaToken,
     readerName: process.env.NEXT_PUBLIC_HONEYPOT_KEY,
